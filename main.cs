e694ffdfe49d1a1457e3255ce1368ee6423889da
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-12 12:05:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function states user's address input.
+        double fahrenheit;
+        double celsius;
+
+        //Input
+        Console.WriteLine("This program will convert temperature.");
+        Console.WriteLine("");
+        Console.Write("Enter fahrenheit: ");
+        fahrenheit = float.Parse(Console.ReadLine());
+
+        //Process
+        celsius = ((fahrenheit - 32) * 5/9);
+
+        //Output
+        Console.WriteLine("");
+        Console.WriteLine("Celsius: " + (celsius.ToString("0.00") + " °C."));
+
+        Console.WriteLine("\nDone.");
+    }
+}
